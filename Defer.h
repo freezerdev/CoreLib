a03a2b364d3a225782fe74ec3fc382f96b79f14f
Brian Friesen <bfriesen@scalable.com>
2026-07-13 21:03:20 -0700

    Add Defer for none RAII types.

---
+++ Defer.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <utility>
+
+NS_BEGIN
+
+template <typename F>
+struct Defer final
+{
+	Defer(F &&fnDefer) : m_fnDefer(std::move(fnDefer)) {}
+	Defer(const Defer &src) = delete;
+	Defer(Defer &&src) = default;		// Move constructor is required
+	~Defer(void) {m_fnDefer();}
+
+	Defer &operator=(const Defer &src) = delete;
+	Defer &operator=(Defer &&src) = delete;
+
+	F m_fnDefer;
+};
+
+// Helper function for C++14 support
+template <typename F>
+Defer<F> MakeDefer(F &&fnDefer)
+{
+	return Defer<F>(std::move(fnDefer));
+}
+
+#define CONCAT_IMPL(a, b) a##b
+#define CONCAT(a, b) CONCAT_IMPL(a, b)
+
+// C++14 version
+#define DEFER(code) auto CONCAT(_defer_, __LINE__) = MakeDefer([&](){code;})
+// C++17 version
+//#define DEFER(code) auto CONCAT(_defer_, __LINE__) = Defer([&](){code;})
+
+NS_END
